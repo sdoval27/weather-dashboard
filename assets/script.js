485c1e2cce4afdde902dec4948ad8f2afb414bf5
unknown <sashardoval@gmail.com>
2023-01-30 17:42:27 -0800

    added styling

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,16 @@
 
-fetch('api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=a400645680064e572aa20f814a90c367', {
-    method: 'GET', //GET is the default.
-    credentials: 'same-origin', // include, *same-origin, omit
-    redirect: 'follow', // manual, *follow, error
-})
-.then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+function getApi(){
+
+  var requestUrl='https://api.openweathermap.org/data/2.5/forecast?q=London,uk&appid=a400645680064e572aa20f814a90c367';
+  var city = $("#searchbar input").val();
+  
+  fetch(requestUrl)
+  .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(city.value)
+      renderFiveDay(data)
+    });
+  
+}
