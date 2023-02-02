40588e07d9c2c1a4fd177afa9e63130596cf967c
unknown <sashardoval@gmail.com>
2023-02-01 23:12:40 -0800

    most recent update

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,6 @@ $(function () {
 
   var todaysDate = dayjs().format('dddd, M/D/YYYY');
   $('#date').text(todaysDate);
-  //5day forecast
 
   //city search function
 
@@ -24,6 +23,17 @@ $(function () {
         renderFiveDay(data);
       });
 
+    var todaysUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey + '&units=imperial'
+    fetch(todaysUrl)
+
+      .then(function (response) {
+        console.log(response);
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        //renderToday(data);
+      });
     saveHistory();
   }
 
@@ -45,10 +55,10 @@ $(function () {
       li.text(cityLi);
       li.attr("data-index", i);
 
-     // var button = document.createElement("button");
-     // button.textContent = "Check Weather";
+      // var button = document.createElement("button");
+      // button.textContent = "Check Weather";
 
-     // li.appendChild(button);
+      // li.appendChild(button);
       savedCities.append(li);
       console.log(savedCities);
     }
@@ -57,41 +67,107 @@ $(function () {
     localStorage.setItem('cityKey', JSON.stringify(searchHistory));
 
     console.log(cityName);
-   
+
   }
+
+  
+  var cityNameEl = $('#city-name');
+  //function renderToday(weather) {
+  //  var cityName = $('#searchbar').val();
+  //  cityNameEl.addClass(".info");
+    //cityNameEl.append(cityName);
+   
+    //tempEl.textContent = "Temp: " + weather.list[0].main.temp + "°F";
+    //tempEl.textContent = "Temp: " + weather.list[0].main.temp + "°F";
+    //tempEl.textContent = "Temp: " + weather.list[0].main.temp + "°F";
+
+  //}
 
   var fiveDayEl = $('.icons-container');
-  var cardDays = $(".icons");
-  var cityNameEl = $('<h2>');
-  
-  function renderFiveDay(weather){
-    console.log(weather);
-    
-    //create for loop of weather list;
-    //weather:calls api data List: calls the list array w weather info
-    //and skips by weather info per day (this is the +8)
-    for(i=0; i < weather.list.length; i = i + 8){
-      console.log(weather.list[i]);
-      var tempEl = document.querySelector('#temp-'+i);
+  var cardDay1 = $("#day-1");
+  var cardDay2 =$('#day-2');
+  var cardDay3 = $('#day-3');
+  var cardDay4 = $('#day-4');
+  var cardDay5 = $('#day-5');
 
-      var humEl = document.querySelector('#hum-'+i);
-      var windEl =document.querySelector('#speed-'+i); //call by id and i
+  function renderFiveDay(weather) {
+    console.log(weather);
+
+    //create for loop of weather list;
+    for (i = 0; i < weather.list.length; i= i + 8) {
+      console.log(weather.list[i]);
+
+      var tempEl1 = $('p');
+      console.log(tempEl1);
+      var tempEl2 = $('p');
+      var tempEl3 = $('p');
+      var tempEl4 = $('p');
+      var tempEl5 = $('p');
+
+      var humEl1 = $('p');
+      var humEl2 = $('p');
+      var humEl3 = $('p');
+      var humEl4 = $('p');
+      var humEl5 = $('p');
+
+      var windEl1 = $('p');
+      var windEl2 = $('p');
+      var windEl3 = $('p'); 
+      var windEl4 = $('p');
+      var windEl5 = $('p');
       //put 5day forecast into div class: icons
-      tempEl.textContent= "Temp: " + weather.list[i].main.temp + "°F";
-      console.log(tempEl);
-      humEl.textContent= "Humidity: " + weather.list[i].main.humidity;
-      windEl.textContent= "Winds: " + weather.list[i].wind.speed + "mph";
+      tempEl1.text("Temp: " + weather.list[0].main.temp + "°F");
+      console.log(tempEl1);
+      humEl1.text("Humidity: " + weather.list[0].main.humidity);
+      windEl1.text("Winds: " + weather.list[0].wind.speed + "mph");
+      //day 2
+      tempEl2.text("Temp: " + weather.list[8].main.temp + "°F");
       
-     //fiveDayEl.text(cardDays);
-     tempEl.innerText;
-     humEl.innerText;
-     windEl.innerText;
-     //cardDays.text(humEl);
-     //cardDays.text(windEl);
-    }
+      humEl2.text("Humidity: " + weather.list[8].main.humidity);
+      windEl2.text("Winds: " + weather.list[8].wind.speed + "mph");
+      //day3
+      tempEl3.text("Temp: " + weather.list[16].main.temp + "°F");
       
-    
+      humEl3.text("Humidity: " + weather.list[16].main.humidity);
+      windEl3.text("Winds: " + weather.list[16].wind.speed + "mph");
+      //day 4
+      tempEl4.text("Temp: " + weather.list[32].main.temp + "°F");
+      
+      humEl4.text("Humidity: " + weather.list[32].main.humidity);
+      windEl4.text("Winds: " + weather.list[32].wind.speed + "mph");
+      // day 5
+      tempEl5.text("Temp: " + weather.list[39].main.temp + "°F");
+      humEl5.text("Humidity: " + weather.list[39].main.humidity);
+      windEl5.text("Winds: " + weather.list[39].wind.speed + "mph");
+
+      
+      
+      cardDay1.append(tempEl1);
+      cardDay1.append(humEl1);
+      cardDay1.append(windEl1);
+
+      
+      cardDay2.append(tempEl2);
+      cardDay2.append(humEl2);
+      cardDay2.append(windEl2);
+
+      
+      cardDay3.append(tempEl3);
+      cardDay3.append(humEl3);
+      cardDay3.append(windEl3);
+
+      
+      cardDay4.append(tempEl4);
+      cardDay4.append(humEl4);
+      cardDay4.append(windEl4);
+
+      
+      cardDay5.append(tempEl5);
+      cardDay5.append(humEl5);
+      cardDay5.append(windEl5);
+
   }
+}
   //assign local storage to searched cities
   $('#searchbtn').on('click', searchCity);
 
@@ -103,10 +179,8 @@ $(function () {
 
 
 
-  //gift from sinclair
+ 
   //dt = eval(data.dt * 1000)
   //today = new Date(dt).toLocaleDateString('en-US')
 
-  //TODOsearch function with city copulation
-  //TODO weather widgets appended to #forecast
 });
